8eae1ef093199eb451d96cbb43f6afde54a08d29
Davydov Roman <rdfrontend1@gmail.com>
2025-10-23 01:01:50 +0300

    day4 - routing - commit & push

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,67 @@ import ReactDOM from "react-dom/client";
 import "./assets/scss/normalize.scss";
 import "./assets/scss/style.scss";
 import { ToDoListPage } from "./pages/ToDoListPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { ToDo } from "./models/todo-item";
+import { NotFound } from "./pages/404";
+import { ItemDescription } from "./pages/ItemDescription";
+import { Layout } from "./layouts/Layout";
+
+const todos: ToDo[] = [
+  {
+    id: 0,
+    text: 'Первое дело',
+    isDone: false
+  },
+  {
+    id: 1,
+    text: 'Второе дело',
+    isDone: true
+  },
+  {
+    id: 2,
+    text: 'Третье дело',
+    isDone: false
+  },
+  {
+    id: 3,
+    text: 'Четвёртое дело',
+    isDone: true
+  }
+]
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage todos={todos} />
+      },
+      {
+        path: '/todo',
+        element: <ToDoListPage />
+      },
+      {
+        path: '/list/:id',
+        element: <ItemDescription todos={todos} />
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoListPage />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
