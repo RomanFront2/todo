e29a1d6e5d3db110de610b327348bc3c1e318e69
Davydov Roman <rdfrontend1@gmail.com>
2025-10-24 11:23:03 +0300

    day 5 - add redux - commit & push

--- src/router.tsx
+++ src/router.tsx
@@ -2,32 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./layouts/Layout";
 import { NotFound } from "./pages/404";
 import { ToDoListPage } from "./pages/ToDoListPage";
-import { ToDo } from "./models/todo-item";
 import { ViewList } from "./pages/ViewList";
 import { ViewListItem } from "./pages/ViewListItem";
-
-const todos: ToDo[] = [
-    {
-        id: 0,
-        text: 'Первое дело',
-        isDone: false
-    },
-    {
-        id: 1,
-        text: 'Второе дело',
-        isDone: true
-    },
-    {
-        id: 2,
-        text: 'Третье дело',
-        isDone: false
-    },
-    {
-        id: 3,
-        text: 'Четвёртое дело',
-        isDone: true
-    }
-]
 
 export const router = createBrowserRouter([
     {
@@ -36,8 +12,8 @@ export const router = createBrowserRouter([
         errorElement: <NotFound />,
         children: [
             { path: '/', element: <ToDoListPage /> },
-            { path: '/list', element: <ViewList todos={todos} /> },
-            { path: '/list/:id', element: <ViewListItem todos={todos} /> }
+            { path: '/list', element: <ViewList /> },
+            { path: '/list/:id', element: <ViewListItem /> }
         ]
     },
     {
---
+++ src/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+import todoReducer from './features/todoList'
+import { loadFromLocalStorage, saveToLocaleStorage } from './helpers/storage'
+
+export const store = configureStore({
+    reducer: {
+        todoList: todoReducer
+    },
+    preloadedState: loadFromLocalStorage()
+})
+
+store.subscribe(() => saveToLocaleStorage(store.getState()))
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
