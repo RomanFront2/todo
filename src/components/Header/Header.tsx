ada404707c3eff081efa68f5d9ac44f912acebb0
Davydov Roman <rdfrontend1@gmail.com>
2025-10-24 20:18:42 +0300

    day 6 - Styled components - commit & push

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,18 @@
 import { NavLink } from "react-router-dom";
 import classes from "./Header.module.scss";
+import { HeaderContainer, HeaderWrapper } from "./Header.styled";
+
 
 export const Header = () => {
   const getActiveClass = ({ isActive }: { isActive: boolean }): string => {
     return isActive ? `${classes.active} ${classes.link}` : classes.link
   }
   return (
-    <header className={classes.header}>
-      <div className={classes.container}>
+    <HeaderWrapper>
+      <HeaderContainer>
         <NavLink to="/" className={getActiveClass}>ToDo</NavLink>
         <NavLink to="/list" className={getActiveClass}>List</NavLink>
-      </div>
-    </header>
+      </HeaderContainer>
+    </HeaderWrapper>
   );
 };
